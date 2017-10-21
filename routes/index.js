2d8bd4089492c64243454ed42cf1a5a685c14e2a
Ronit Sharma <sharmaronit009@gmail.com>
2017-10-21 15:59:22 -0700

    added fuzzy search

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,33 @@
-var express = require('express');
-var router = express.Router();
-var FuzzySearch = require('fuzzy-search');
+const express = require('express');
+const router = express.Router();
+const FuzzySearch = require('fuzzy-search');
+const stocks = require('../res/stocks.json');
+const format = require('format-number');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/newstock', (req, res) => {
-  var people = [
-    {
-      name: {
-        firstName: 'Jesse',
-        lastName: 'Bowen',
-      },
-      state: 'Seattle',
-    }
-  ];
-  
-  var fuzzy = new FuzzySearch(people, ['name.firstName', 'state'], {
-    caseSensitive: true,
+  var names = [];
+  for (let num in stocks) {
+    let company = {};
+    company.name = stocks[num].Symbol
+    company.marketCap = format({prefix: '$', integerSeparator: ','})(parseInt((stocks[num].MarketCap)/1000)*1000);
+    company.companyName = stocks[num].Name
+    names.push(company);
+  }
+  res.render('newstock', {names});
+});
+
+router.get('/stocksearch', (req, res) => {
+  var fuzzy = new FuzzySearch(stocks, ['Symbol'], {
+    caseSensitive: false,
   });
-  var result = fuzzy.search('ess');
-  res.render('newstock', { result: result[0].name.firstName });
+  var result = fuzzy.search(req.query.stock);
+  res.type('json');
+  res.send(result);
 });
 
 module.exports = router;
